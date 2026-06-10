7c6cf8c8653314084618e61d1f193410721885d7
Proxy <dheerajreddy0535@gmail.com>
2026-06-10 17:01:51 +0530

    Update orders.sql

--- orders.sql
+++ orders.sql
@@ -1,8 +1,9 @@
-create or replace table orders (
-  order_id int,
-  product_id int,
-  product_name varchar,
-  employee_id int,
-  quantity int,
-  total_amount int
+CREATE OR REPLACE TABLE orders (
+    order_id INT,
+    customer_id INT,
+    product_id INT,
+    product_name VARCHAR,
+    employee_id INT,
+    quantity INT,
+    total_amount INT
 );
