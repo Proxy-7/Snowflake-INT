993c32dd8cc8951984cf46a8589c6a4895a462d4
Proxy <dheerajreddy0535@gmail.com>
2026-06-10 17:09:01 +0530

    Create insert_orders.sql

---
+++ insert_orders.sql
@@ -0,0 +1,13 @@
+INSERT INTO orders
+(order_id, customer_id, product_id, product_name, employee_id, quantity, total_amount)
+VALUES
+(1001, 101, 1, 'p1', 1, 2, 60),       -- Rahul buys 2 p1
+(1002, 102, 2, 'p2', 2, 3, 150),      -- Priya buys 3 p2
+(1003, 103, 3, 'p3', 3, 1, 6000),     -- Arjun buys 1 p3
+(1004, 104, 1, 'p1', 1, 5, 150),      -- Sneha buys 5 p1
+(1005, 105, 2, 'p2', 2, 4, 200),      -- Kiran buys 4 p2
+(1006, 101, 3, 'p3', 3, 2, 12000),    -- Rahul buys 2 p3
+(1007, 102, 1, 'p1', 1, 10, 300),     -- Priya buys 10 p1
+(1008, 103, 2, 'p2', 2, 1, 50),       -- Arjun buys 1 p2
+(1009, 104, 3, 'p3', 3, 3, 18000),    -- Sneha buys 3 p3
+(1010, 105, 1, 'p1', 1, 7, 210);      -- Kiran buys 7 p1
