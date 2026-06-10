6ee35af38894fcc18203cbd4a0d7c452cb370c23
Proxy <dheerajreddy0535@gmail.com>
2026-06-10 16:27:08 +0530

    Update insert_rows.sql

--- insert_rows.sql
+++ insert_rows.sql
@@ -1,2 +1,2 @@
-insert into emp values (1,a,1000),(2,b,2000),(3,c,3000);
-insert into products values (1,p1,30),(2,p2,50),(3,p3,6000);
+insert into emp values (1,'a',1000),(2,'b',2000),(3,'c',3000);
+insert into products values (1,'p1',30),(2,'p2',50),(3,'p3',6000);
